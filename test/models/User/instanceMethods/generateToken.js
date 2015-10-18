4108a7e49e095f758f145261a17ef97e20c42122
Adam Mooney <adamlukemooney@gmail.com>
2015-10-18 18:57:04 +0200

    Write tests for User#generateToken, improve tests for User#checkPassword

--- test/models/User/instanceMethods/generateToken.js
+++ test/models/User/instanceMethods/generateToken.js
@@ -3,21 +3,23 @@ require('../../../support/common');
 var userTestSupport = require('../support/common');
 var moment = require('moment');
 
-var User = global.db.User;
-
 describe('User#generateToken', function() {
 
   var exampleUser;
 
   beforeEach(function() {
     exampleUser = userTestSupport.buildExampleAdminUser();
-    return exampleUser.save();
   });
 
-  it('returns a token string and an expiry time in the future', function() {
+  it('returns a JSON web token as a string', function() {
     var result = exampleUser.generateToken();
     expect(result.token).to.be.a('string');
-    expect(result.expiry.isAfter(moment())).to.equal(true);
+  });
+
+  it('returns a valid future expiry time', function() {
+    var result = exampleUser.generateToken();
+    expect(moment(result.expiry).isValid()).to.eq(true);
+    expect(moment(result.expiry).isAfter(moment())).to.eq(true);
   });
 
 });
